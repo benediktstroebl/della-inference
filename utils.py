b855a2b0ac90410bd25b44b4d148b7bc8fed9cf1
Veniamin Veselovsky <venia@veselovsky.org>
2024-09-17 14:06:01 -0400

    (1) quantization (2) change to pli

--- utils.py
+++ utils.py
@@ -4,9 +4,9 @@ from jinja2 import Template
 import os
 
 def convert_yaml_to_slurm(cfg: DictConfig):
-    azure = cfg['azure']
+    use_azure = cfg['use_azure']
 
-    if azure:
+    if use_azure:
         slurm_path = os.path.join(cfg['slurm_dir'], cfg['slurm_azure_fname'])
     else:
         slurm_path = os.path.join(cfg['slurm_dir'], cfg['slurm_fname'])
@@ -17,6 +17,12 @@ def convert_yaml_to_slurm(cfg: DictConfig):
     template = Template(slurm_file)
     slurm_script = template.render(**cfg['model_configs'])
 
+    use_pli = cfg['use_pli']
+    if use_pli:
+        list_slurm = slurm_script.split('\n')
+        list_slurm.insert(3, '#SBATCH --partition=pli')
+        list_slurm.insert(4, '#SBATCH --account=hal')
+        slurm_script = '\n'.join(list_slurm)
     return slurm_script, slurm_path
 
 # just for testing
